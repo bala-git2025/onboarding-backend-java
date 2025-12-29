e69cc9f2c2a4bde99d3453f76d026303d313ead9
Vivek Singh <v.q.kumar.singh@accenture.com>
2025-12-29 16:22:28 +0530

    Initial code commit on behalf of Suraj

---
+++ src/main/java/com/accenture/onboarding_backend/OnboardingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.onboarding_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/onboarding_backend/OnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.onboarding_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
